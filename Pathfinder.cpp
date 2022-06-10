39fc24a14f52e0d3a8afed0ef3b54d53f461397e
Morozov <morozov.aa@cet-mipt.ru>
2022-06-10 13:22:28 +0300

    Добавил TimeChecker

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 #include "XPSHelper\XPSHelper.hpp"
+#include "TimeChecker\TimeChecker.hpp"
 
 int main(int argc, char* argv[]) {
+	TimeChecker checkTime;
+	auto checkTime2 = checkTime;
 	XPSHelper xps(argv[1]);
+
 
 	return 0;
 }
